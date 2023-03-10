7d2afb65b864fa37eaa1c946b85c47e10af7bdf2
ddaniel-bit <67413475+ddaniel-bit@users.noreply.github.com>
2023-03-10 15:32:24 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fajlNev = "naplo.csv";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OsztalyokBetoltese(string fajlNev)
+        {
+            List<Osztalyzat> jegyek = new List<Osztalyzat>();
+            jegyek.Clear();
+            StreamReader sr = new StreamReader(fajlNev);
+            while (!sr.EndOfStream)
+            {
+                string[] mezok = sr.ReadLine().Split(";");
+                Osztalyzat ujJegy = new Osztalyzat(mezok[0], mezok[1], mezok[2], int.Parse(mezok[3]));
+                jegyek.Add(ujJegy);
+            }
+            sr.Close();
+            dgNaplo.ItemsSource = jegyek;
+            MessageBox.Show("Az állomány beolvasása");
+        }
+
+        private void sliJegy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblJegy.Content = sliJegy.Value;
+        }
+
+        private void btnBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OsztalyokBetoltese(fajlNev);
+        }
+
+        private void btnRogzit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Osztalyzat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Osztalyzat
+    {
+        String nev;
+        String datum;
+        String tantargy;
+        int jegy;
+
+        public Osztalyzat(string nev, string datum, string tantargy, int jegy)
+        {
+            this.nev = nev;
+            this.datum = datum;
+            this.tantargy = tantargy;
+            this.jegy = jegy;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Datum { get => datum; }
+        public string Tantargy { get => tantargy; }
+        public int Jegy { get => jegy; }
+    }
+}
